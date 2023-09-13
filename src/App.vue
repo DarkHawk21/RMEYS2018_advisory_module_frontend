418eed3f04b6c118d9def0c7e71186a848a22126
Enrique Carranza <enriquecarranza-1999@live.com.mx>
2023-09-12 19:34:42 -0600

    Header ready

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>
-    <h1>Plataforma RMEYS2018</h1>
-  </div>
+  <header>
+    <div class="header_logo_container">
+      <img src="/images/logo_unam.png" alt="Logotipo UNAM">
+    </div>
+
+    <div class="header_titles_container">
+      <h1>Alumno</h1>
+      <h2>Calendario de Asesorías</h2>
+    </div>
+
+    <div class="header_logo_container">
+      <img src="/images/logo_enp9.png" alt="Logo ENP 9">
+    </div>
+  </header>
+
+  <RouterView />
 </template>
+
+<script setup>
+  import { RouterLink, RouterView } from 'vue-router';
+</script>
