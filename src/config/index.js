9f2082682a54bde264b59e0e9f7a684df9d02bd4
Enrique Carranza <enriquecarranza-1999@live.com.mx>
2023-10-13 20:01:24 -0600

    Modifying api route

--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,2 @@
 export const _TOKEN = 'token-rmeys2018-module';
-export const _API_URL = 'http://mediateca.prepa9.unam.mx/asesorias/api_index/index.php/api/v1';
+export const _API_URL = 'http://mediateca.prepa9.unam.mx/asesorias/api/api/v1';
