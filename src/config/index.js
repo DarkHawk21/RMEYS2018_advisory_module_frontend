f47f0fb3ce5e122518aaa78eacad99bfeea31989
Enrique Carranza <enriquecarranza-1999@live.com.mx>
2026-01-14 13:55:44 -0600

    Fix url para api

--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,2 @@
 export const _TOKEN = 'token-rmeys2018-module';
-export const _API_URL = 'https://mediateca.prepa9.unam.mx/asesorias/api/api/v1';
+export const _API_URL = 'https://mediateca.prepa9.unam.mx/asesorias/api/index.php/api/v1';
